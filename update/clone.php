35c08b6a5cc2355fbfc063e03fc34c3b3bda9ae3
Husani Santos <husanisantos@gmail.com>
2023-04-30 19:39:55 -0300

    Erros no update

--- update/clone.php
+++ update/clone.php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $url = $_SERVER['HTTP_REFERER'];
     $date = date('Y-m-d H:i:s');
 
-    $log_file = 'log.txt';
+    $log_file = '../logs/update.txt';
     $log_message = "{$date} - {$url}\n";
     file_put_contents($log_file, $log_message, FILE_APPEND);
 
