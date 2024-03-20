5f747b914e46e12e5b84369f3c496b7f1e01588b
João Augusto Lissoni Lanjoni <joaoaugustolanjoni@gmail.com>
2024-03-20 18:23:49 -0300

    fix: ifsp logo

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,14 @@ export default function Home() {
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://vtp.ifsp.edu.br/"
             target="_blank"
             rel="noopener noreferrer"
           >
             Realização{" "}
             <Image
               src="/ifsp.png"
-              alt="Vercel Logo"
+              alt="IFSP Votuporanga Logo"
               className=""
               width={150}
               height={48}
